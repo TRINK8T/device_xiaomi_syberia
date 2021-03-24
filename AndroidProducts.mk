fa6d7a58220f859205bd0406574f02c6f5bd8f8e
TheHitMan7 <krtik.vrma@gmail.com>
2021-03-24 22:33:49 +0530

    ginkgo: Initial SyberiaOS bringup
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/syberia_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    syberia_ginkgo-user \
+    syberia_ginkgo-userdebug \
+    syberia_ginkgo-eng
